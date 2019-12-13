d8a5e2ef75f24184ecc90b1fdc37a4119b99bbfd
Conor Toomb <conor.toomb114@gmail.com>
2019-12-13 15:00:28 -0500

    Final Draft Pending Test

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Javascript-Homework
-This project is a basic Password Generator formatted in html, designed in css, and scripted and special format detail done in javascript.
+This assignment is a basic Password Generator 
+
+I mainly worked with script.js for this homework
+
+I was sure to make specific and different (choice, not necessity) variables for the numbers, special characters, upper case letters, and lower case letters. I made them into arrays by using .split
+
+I put all variables with conditions into a function for generate password to fix the issues
+
+The variable for passwordLength had to be changed into a number by using parseInt. to fix the problem of all password possiblities from coming out in the generator
+
+The loop was created to emphasize the amount of times for the character length
+
+At the end of the function, I returned the newPassword with .join(''); to create the actual password, rather than just 
+
+My Final edit included an added "copy to the clipboard" button and function
--- css/script.js
+++ css/script.js
@@ -1,7 +1,6 @@
 var generateBtn = document.querySelector("#generate");
+var copyBtn = document.querySelector("#copy");
 
-
-// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -11,7 +10,7 @@ function writePassword() {
   copyBtn.removeAttribute("disabled");
   copyBtn.focus();
 }
-//Vars for characters used to make up generated password
+
 var numbers = "0123456789";
 var numberArr = numbers.split("");
 var lowerCase = "abcdefghijklmnopqrstuvwxyz";
@@ -22,70 +21,75 @@ var special = "!@#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
 var specialArr = special.split("");
 
 var newPassword = [];
+var characterBankArr = [];
 
-// Create Prompt for user to choose 8-128 characters for their password
 function generatePassword() {
   var passwordLength = prompt("How many characters would you like in your password? Please choose a number from 8-128");
 
+  passWordLength = parseInt(passwordLength);
   if (passwordLength >= 8 && passwordLength <= 128) {
     alert("Thank you");
   }
-  else {
-    alert("Tsk Tsk! Try again!");
-  } //need to make loop so person has to type correct amount
-  console.log(passwordLength);
 
-  //Create confirm for user to choose if they would like numbers
   var userNum = confirm("Would you like numbers in your password?");
 
   if (userNum === true) {
     alert("Your password will contain numbers.");
+    characterBankArr.push(numberArr);
   }
   else {
     alert("Your password will not contain numbers.");
   }
-  console.log(userNum);
-  // Create Prompt for user to choose if they would like special characters
 
   var userSpecialChar = confirm("Would you like special characters?");
 
   if (userSpecialChar === true) {
     alert("Your password will contain special characters.");
+    characterBankArr.push(specialArr);
   }
   else {
     alert("Your password will not contain special characters.");
   }
-  console.log(userSpecialChar);
-
-  // Create confirm for user to choose if they would like lowercase letters
 
   var userLowerCase = confirm("Would you like lowercase letters in your password?");
 
   if (userLowerCase === true) {
     alert("Your password will contain lowercase letters.");
+    characterBankArr.push(lowerCaseArr);
   }
   else {
     alert("Your password will not contain lowercase letters.");
   }
-  console.log(userLowerCase);
 
   var userUpperCase = confirm("Would you like uppercase letters in your password?");
 
   if (userUpperCase === true) {
     alert("Your password will contain uppercase letters.");
+    characterBankArr.push(upperCaseArr);
   }
   else {
     alert("Your password will not contain uppercase letters.");
   };
-  console.log(userUpperCase);
 
-  newPassword.push(numberArr, lowerCaseArr, upperCaseArr, specialArr);
+  for (var i = 0; i < passwordLength; i++) {
+
+    var characterTypeArr = characterBankArr[Math.floor(Math.random() * characterBankArr.length)];
+
+    var charPicked = characterTypeArr[Math.floor(Math.random() * characterTypeArr.length)];
+    newPassword.push(charPicked);
+  }
 
   return newPassword.join('');
 };
 
-
 function copyToClipboard() {
-}
+  var copyText = document.querySelector("#password");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999);
+  document.execCommand("copy");
+  alert("Copied the text: " + copyText.value);
+};
 
 generateBtn.addEventListener("click", writePassword);
+
+copyBtn.addEventListener("click", copyToClipboard);
